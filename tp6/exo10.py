9be3d1d63e8e8524536177e3dbc3b284695a480c
nordinamb <nordinamb123@gmail.com>
2025-01-21 20:48:22 +0100

    tp6 uploaded

--- tp6/exo10.py
+++ tp6/exo10.py
@@ -5,14 +5,13 @@ from exo9 import get_positive_integer
 
 def main():
     try:
-        # Demander le fichier
+        
         filename = input("Entrez le nom du fichier : ")
         content = read_file(filename)
-        if content:  # Si le contenu a été lu avec succès
+        if content:
             print(f"Contenu du fichier '{filename}' :")
             print(content)
 
-        # Demander un entier positif
         number = get_positive_integer()
         print(f"Vous avez saisi l'entier : {number}")
 
